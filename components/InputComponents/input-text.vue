d8f09c3636b50e83e352dfa714aa75937195218a
WhaletechTHasegawa <82626719+WhaletechTHasegawa@users.noreply.github.com>
2022-03-16 01:30:51 +0900

    ベースクラス作成

---
+++ components/InputComponents/input-text.vue
@@ -0,0 +1,18 @@
+<template>
+  <input-base>
+    <template v-slot:input>
+      <input :class="classes.input" type="input" v-model="inputValue" />
+    </template>
+  </input-base>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import InputBase from '@/classes/input-base'
+
+@Component({})
+export default class InputText extends InputBase {
+  public validation(value: string): void {
+    this.message = value === '' ? '必須入力です' : ''
+  }
+}
+</script>
