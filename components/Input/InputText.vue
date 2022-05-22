3ac9a901d4a5982766f7d501fc4269f3ff1aedd9
WhaletechTHasegawa <82626719+WhaletechTHasegawa@users.noreply.github.com>
2022-05-23 00:44:14 +0900

    title:ファイル整理
    message:フォルダ配置、クラス記述の整理

---
+++ components/Input/InputText.vue
@@ -0,0 +1,22 @@
+<template>
+  <InputBase>
+    <template v-slot:input>
+      <input :class="classes.input" type="input" v-model="inputValue" />
+    </template>
+  </InputBase>
+</template>
+<script>
+import InputBase from '@/Classes/InputBase'
+
+export default {
+  components: {
+    InputBase,
+  },
+
+  data() {
+    return {
+      inputValue: String,
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,11 @@
 <template>
-  <v-card class="mx-auto" width="300" outlined>
-    <v-card-title>My samples</v-card-title>
-    <v-list>
-      <v-list-group :value="false" no-action sub-group>
-        <template v-slot:activator>
-          <v-icon>mdi-application-brackets</v-icon>
-          <v-list-item-title>Components</v-list-item-title>
-        </template>
-
-        <v-list-item
-          v-for="({ title, icon, to }, i) in components"
-          :key="i"
-          :to="to"
-          link
-        >
-          <v-icon v-text="icon"></v-icon>
-          <v-list-item-title v-text="title"></v-list-item-title>
-        </v-list-item>
-      </v-list-group>
-    </v-list>
-  </v-card>
+  <Tutorial/>
 </template>
+
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import Vue from 'vue'
 
-type contents = {
-  icon: string
-  title: string
-  to: string
-}
-
-@Component
-export default class Index extends Vue {
-  public components: contents[] = [
-    {
-      icon: 'mdi-pencil',
-      title: 'Input text',
-      to: '/',
-    },
-    {
-      icon: 'mdi-form-select',
-      title: 'Select box',
-      to: '/',
-    },
-  ]
-}
+export default Vue.extend({
+  name: 'IndexPage'
+})
 </script>
