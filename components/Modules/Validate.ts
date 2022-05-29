1c6d358c3be5fdd5459219a6d39133d0ca2d7522
WhaletechTHasegawa <82626719+WhaletechTHasegawa@users.noreply.github.com>
2022-05-29 19:38:38 +0900

    title:ビルドコミット
    massege:ビルド分をコミット

--- components/Modules/Validate.ts
+++
@@ -1,9 +0,0 @@
-namespace Rules{
-    export const REQUIRED = 'required';
-    export const ALPHA_ONLY = 'alpha-only';
-    export const MAX_LENGTH = 'max-length';
-    export const MIN_LENGTH = 'min-length';
-    export const POSTAL_CODE = 'postal-code';
-}
-
-export default Rules;
--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  router: {
+    base: '/NuxtClassSample/'
+  },
+
+  generate: {
+    dir: 'docs'
   }
 }
