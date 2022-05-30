fdbaab1616e7e9c6510010dda56f632a910c8c70
Takatune <0ta5274738t4p4r@gmail.com>
2022-05-31 00:43:20 +0900

    Create Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <Tutorial/>
+  <div class="m-6 p-4 border-solid border-2 border-gray-600 rounded-md bg-gray-700 text-blue-500">
+    <h1 class="font-bold text-center">Index</h1>
+    <div class="grid-cols-6">
+      <nuxt-link
+        class="py-2 px-24 text-sm bg-gray-800 font-semibold rounded-md shadow-lg focus:outline-none border-double border-4 border-blue-500"
+        to="/Examples/Chain"
+      >
+        Chain
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: 'Home',
+});
 </script>
