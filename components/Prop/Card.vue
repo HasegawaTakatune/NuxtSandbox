fa181f3551e95dc2c1e699e683bc2436f490175d
Takatune <0ta5274738t4p4r@gmail.com>
2022-06-10 01:51:54 +0900

    title:タイトル変更
    message:〃

--- components/Prop/Card.vue
+++ components/Prop/Card.vue
@@ -24,7 +24,7 @@ export default Vue.extend({
 <template>
   <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div>
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Input values</h5>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Component</h5>
     </div>
 
     <input-frame type="text" label="Inpu Text" :value="inputText" />
--- pages/Examples/Prop/index.vue
+++ pages/Examples/Prop/index.vue
@@ -38,7 +38,7 @@ export default Vue.extend({
     </div>
 
     <div>
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Props values</h5>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Page</h5>
       Text: {{ inputText }}
       <br />
       Checkbox: {{ ShowInputCheckbox }}
