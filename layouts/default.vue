9528b2b50e05adbc4020034aabfd0a679d3d19c0
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2022-06-21 23:03:32 +0900

    title:メタ設定
    message:〃

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,21 @@ export default Vue.extend({
 </script>
 <template>
   <html lang="ja">
-    <head> </head>
+    <head>
+      <meta charset="utf-8" />
+      <title>NuxtSandbox</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="description" content="Nuxtプロジェクトのサンプル広場" />
+      <meta name="keywords" content=",,," />
+      <meta name="application-name" content="NuxtSandbox" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="NuxtSandbox" />
+      <meta property="og:image" content="https://hasegawatakatune.github.io/NuxtSandbox/reryka_logo_plane.png" />
+      <meta name="twitter:card" content="summary" />
+      <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
+      <link rel="icon" type="image/x-icon" href="https://hasegawatakatune.github.io/NuxtSandbox/reryka_logo_plane.png" />
+    </head>
     <div>
-      <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
       <Nuxt />
     </div>
   </html>
