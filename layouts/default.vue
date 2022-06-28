033dc56ec27a9a79ead484a1da3530154f9d9d75
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2022-06-29 00:17:56 +0900

    title: メタタグの整理
    message:〃

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,16 +14,6 @@ export default Vue.extend({
 <template>
   <html lang="ja">
     <head>
-      <!-- <meta charset="utf-8" />
-      <title>NuxtSandbox</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <meta name="description" content="Nuxtプロジェクトのサンプル広場" />
-      <meta name="keywords" content=",,," />
-      <meta name="application-name" content="NuxtSandbox" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="NuxtSandbox" />
-      <meta property="og:image" content="https://hasegawatakatune.github.io/NuxtSandbox/reryka_logo_plane.png" />
-      <meta name="twitter:card" content="summary" /> -->
       <link rel="icon" type="image/x-icon" href="https://hasegawatakatune.github.io/NuxtSandbox/reryka_logo_plane.png" />
       <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
     </head>
